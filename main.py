be89dd5083b70f6b45cea0c07e7275998912b356
ZaydahCodes <sanusizaidat@gmail.com>
2023-06-15 06:13:07 +0100

    Checking if the player has won, making code recursive

--- main.py
+++ main.py
@@ -2,22 +2,30 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-
+length = len(chosen_word)
 print(f'The solution is {chosen_word}.')
 
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the
+# letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+
 display = []
-for i in range(len(chosen_word)):
+for i in range(length):
     display += "_"
 
-guess = input("Please guess a letter: \n").lower()
+endgame = False
+for item in range(length):
+    while not endgame:
+        guess = input("Please guess a letter: \n").lower()
 
-if guess in chosen_word:
-    print("Right")
-else:
-    print("Wrong")
+        if guess in chosen_word:
+            print("Right")
+        else:
+            print("Wrong")
 
-for i in range(len(display)):
-    if chosen_word[i] == guess:
-        display[i] = guess
+        for i in range(length):
+            if chosen_word[i] == guess:
+                display[i] = guess
 
-print(display)
+        if "_" not in display:
+            endgame = True
+        print(display)
