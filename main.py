09f7fe3a6e1d057d6f6ba831681c05001c980c98
ZaydahCodes <sanusizaidat@gmail.com>
2023-06-15 08:41:56 +0100

    keeping track of player lives

--- main.py
+++ main.py
@@ -1,12 +1,69 @@
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 length = len(chosen_word)
+lives = 6
+
 print(f'The solution is {chosen_word}.')
 
-#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the
-# letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
 
 display = []
 for i in range(length):
@@ -17,15 +74,18 @@ for item in range(length):
     while not endgame:
         guess = input("Please guess a letter: \n").lower()
 
-        if guess in chosen_word:
-            print("Right")
-        else:
-            print("Wrong")
+        if guess not in chosen_word:
+            lives -= 1
+            if lives == 0:
+                endgame = True
+                print("You lose")
+
 
         for i in range(length):
             if chosen_word[i] == guess:
                 display[i] = guess
-
+        print(f"{''.join(display)}")
         if "_" not in display:
             endgame = True
-        print(display)
+            print("You Win")
+        print(stages[lives])
